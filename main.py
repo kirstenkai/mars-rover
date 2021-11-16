30578a98313941ac2d30742b212fb62225490e0f
KC Kai <kirstenckai@gmail.com>
2021-11-15 16:34:36 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+# read file input
+
+with open('instructions.txt', 'r') as instructions:
+  lines = instructions.readlines()
+
+# store plateau coordinate in var
+plateau_size = lines.pop(0).strip().split()
+
+cardinal_degrees = {'N': 0, 'E': 90, 'S': 180, 'W': 270}
+cardinal_degrees_reverse = {v: k for k, v in cardinal_degrees.items()}
+
+def calculate_position(x, y, h):
+  if h == 'N':
+    position = (x, y+1)
+  elif h == 'S':
+    position = (x, y-1)
+  elif h == 'E':
+    position = (x+1, y)
+  elif h == 'W':
+    position = (x-1, y)
+  return position
+
+# define a function to determine the rover's orientation
+# L and R instructions determine what the rover's heading will be
+# if the move is M ignore it
+# directions for each rover should be separated
+def calculate_heading(h, turn):
+  cardinal_degree = cardinal_degrees[h]
+  if turn == 'L':
+    new_degree = cardinal_degree - 90
+  elif turn == 'R':
+    new_degree = cardinal_degree + 90
+  new_degree = new_degree % 360
+  return cardinal_degrees_reverse[new_degree]
+
+def rover(start, directions):
+  x, y, h = start
+  for d in directions:
+    if d == 'M':
+      x, y = calculate_position(x, y, h)
+    else:
+      h = calculate_heading(h, d)
+  print(x, y, h)
+
+# iterate over instructions, pass start and direction to rover
+for i in range(0, len(lines)-1, 2):
+  start = lines[i].strip()
+  x, y, h = start.split(' ')
+  x = int(x)
+  y = int(y)
+  directions = lines[i+1].strip()
+  rover((x, y, h), directions)
+
+
+
+
+
+
+# return final coordinates of all rovers
